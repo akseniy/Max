21466af2ab2b81bcb749fb28f2b7c661389f048c
Artem <91414638+artyomyarukov@users.noreply.github.com>
2025-11-20 13:00:31 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,7 +6,6 @@ from config.config import Config, load_config
 from handlers.handlers import base_router
 import asyncpg
 
-
 async def init_database(pool):
     """Инициализирует базу данных - создает таблицы если их нет"""
     try:
@@ -75,7 +74,6 @@ async def init_database(pool):
         print(f"❌ Ошибка при инициализации базы данных: {e}")
         raise
 
-
 async def create_pool_with_retry(config, retries=10, delay=3):
     """Создаём подключение к базе с повторными попытками"""
     for i in range(retries):
@@ -88,13 +86,15 @@ async def create_pool_with_retry(config, retries=10, delay=3):
                 port=config.db.db_port
             )
             print("✅ Подключение к базе успешно")
+            
+           
+            await init_database(pool)
+            
             return pool
         except Exception as e:
-            print(f"⚠️ База данных не готова, попытка {i+1}/{retries}...")
+            print(f"⚠️ База данных не готова, попытка {i+1}/{retries}... Ошибка: {e}")
             await asyncio.sleep(delay)
     raise Exception("❌ Не удалось подключиться к базе после нескольких попыток")
-
-
 
 async def main():
     logging.basicConfig(level=logging.INFO, filename='py_log.log', filemode='w')
